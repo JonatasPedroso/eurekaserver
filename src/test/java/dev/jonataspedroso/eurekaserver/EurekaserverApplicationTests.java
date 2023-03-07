bfd09464b5c4189052ffddb515749ac86924d449
Jonatas Pedroso <jonatasarlindo@hotmail.com>
2023-03-07 08:39:28 -0300

    Adicionando configurações iniciais

---
+++ src/test/java/dev/jonataspedroso/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jonataspedroso.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
